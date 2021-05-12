29c82bd40ac75356d8d6fcc85c9df84b6e3a2e04
ninaspray <ninaspray@gmail.com>
2021-05-12 20:27:51 +0100

    Added proptypes to property card

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,7 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const PropertyCard = () => {
-  return <div className="AddProperty">Property Card</div>;
+const PropertyCard = (props) => {
+  const { title, type, bathrooms, bedrooms, price, city, email } = props;
+
+  return (
+    <div className="propertyCard">
+      <span className="title">{title}</span>
+      <span className="type">{type}</span>
+      <span className="bathrooms">{bathrooms} bathroom(s)</span>
+      <span className="bedrooms">{bedrooms} bedroom(s)</span>
+      <span className="price">£{price}</span>
+      <span className="city">{city}</span>
+      <span className="email">Contact: {email}</span>
+    </div>
+  );
 };
-
 export default PropertyCard;
+
+PropertyCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  bathrooms: PropTypes.number.isRequired,
+  bedrooms: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  city: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
